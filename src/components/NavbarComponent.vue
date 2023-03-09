0ab102f12e15f10e029afef116340a060a4ced99
Mathias Odllund <mathiasodlund@proton.me>
2023-03-09 09:30:27 +0100

    Chore (Tailwind) Introuduced Vue router and registered DaisyUI

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="navbar bg-base-100 shadow-md">
+    <a class="btn btn-ghost normal-case text-xl">Logo</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
